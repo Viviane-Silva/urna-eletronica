cd068bea04e9807e053e7f77669df1250bc7d7ba
Viviane-Silva <viviane.leite.silva@gmail.com>
2021-04-23 13:46:44 -0300

    json

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: "VEREADOR",
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 32566,
+                nome: "Tal Tal",
+                partido: "Oq",
+                fotos:[
+                    {url:'', legenda: "Vereador"}
+                ]
+            },
+            {
+                numero:77222,
+                nome: "Moacir",
+                partido: "Opa",
+                foto:[
+                    {url:'', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos:[
+            {
+                numero:99,
+                nome:'Gil',
+                partido: 'Oq',
+                vice: 'Agnaldo',
+                fotos: [
+                    {url:'', legenda: 'Prefeito',
+                    {url:'', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 85,
+                nome: 'Zulema',
+                partido: 'Opa',
+                vice: "Raco",
+                fotos: [
+                    {url:'', legenda: 'Prefeito',
+                    {url:'', legenda: 'Vice-Prefeito', small: true}
+                ]
+
+            },
+        ]
+    }
+];
