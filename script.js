1b1c7b33618bb03b0b97eb7d2f097b9b919c8b50
Viviane-Silva <viviane.leite.silva@gmail.com>
2021-04-24 11:56:43 -0300

    correção

--- script.js
+++ script.js
@@ -8,12 +8,16 @@ let numeros = document.querySelector('.d-1-3');
 
 let etapaAtual = 0;
 let numero = '';
+let votoBranco = false;
+
 
 function inicioEtapa() {
 
     let etapa = etapas[etapaAtual];
 
     let numeroHtml ='';
+    numero = '';
+    votoBranco = false;
 
     for(let i=0;i<etapa.numeros;i++){
         if(i===0){
@@ -83,19 +87,30 @@ function clicou (n) {
 }
 
 function branco () {
-    alert ('Clicou em BRANCO!');
+    if (numero === ''){
+        votoBranco = true;
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        numero.innerHTML = '';
+        descricao.innerHTML = '<div class="aviso--grande pisca">VOTO EM BRANCO</div>';
+    } else {
+           alert("Para votar em BRANCO, não pode ter digitado nehum número");
+    }
     
 }
 
 function corrige () {
-    alert ('Clicou em CORRIGE!');
+    inicioEtapa();
     
 }
 
 function confirma () {
-    alert ('Clicou em CONFIRMA!');
+    if (votoBranco === true){
+            console.log("Confirmando como BRANCO...");      
+    }else if (numero.lenght === etapa.numeros){
+        console.log("Confirmando como "+numero);
+    }
      
 }
-
 
 inicioEtapa ();
